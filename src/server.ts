f849ab2deab34da0ebeb996b794cc4f224d4ce6f
Carlos Godoi <cgodoi@Carlos-MBP.bbrouter>
2022-10-03 23:05:43 -0300

    Criando projeto com TypeScript

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (request, response) => {
+  return response.json({
+    message: 'Hello World Ignite',
+  });
+});
+
+app.listen(3333);
